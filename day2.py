e523634994312725458f4728029c18b9b49c6fbc
Shreya <253979927+shreya-sp26@users.noreply.github.com>
2026-07-22 18:37:32 +0530

    day2.py

---
+++ day2.py
@@ -0,0 +1,33 @@
+# Variables
+name = "Shreya"
+age = 19
+print(name)
+print(age)
+
+# Data Types
+a = 10
+b = 2.5
+c = "AI"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# Operators
+x = 10
+y = 5
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+
+# For Loop
+for i in range(1, 6):
+    print(i)
+
+# Function
+def greet(name):
+    print("Hello", name)
+
+greet("Shreya")
